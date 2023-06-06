698e6d171c1d3d07b57e274fdee2cb8a7ab3f089
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-06-06 20:42:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ import pyrogram
 from pyrogram import Client
 from pyrogram import enums
 
-BOT_TOKEN = environ.get("BOT_TOKEN", "5010662156:AAEzSoh61W1KxaWKE4HfjykqmT1apHIKXps")
-DEPO = environ.get("DEPO", "kcjdjfjfjrjrjrjrjjrjrje")
+BOT_TOKEN = environ.get("BOT_TOKEN", "5021AAEzSoh61W1KxaWKE4HfjykqmT1apHIKXps")
+DEPO = environ.get("DEPO", "kcjrjrjrjjrjrje")
 OWNER_ID = "mmagneto"
-APP_ID = int(environ.get("APP_ID", 2374174))
-API_HASH = environ.get("API_HASH", "c23db4aa92da73ff603666812268597a")
+APP_ID = int(environ.get("APP_ID", 23174))
+API_HASH = environ.get("API_HASH", "aa92da73ff603666812268597a")
 DOWNLOAD_DIR = 'downloads'
